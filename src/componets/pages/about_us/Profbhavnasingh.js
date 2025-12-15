bd567ed1a1ea228fd8232fa8019e4d926be04921
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-12-15 14:48:31 +0530

    Profbhavnasingh page done

--- src/componets/pages/about_us/Profbhavnasingh.js
+++ src/componets/pages/about_us/Profbhavnasingh.js
@@ -1,72 +1,137 @@
-import React from 'react';
+import React from "react";
 
 // Import images at the top
-import AboutImg from '../../../assets/images/about-img.png';
-import BgShape2 from '../../../assets/images/bg-shape2.png';
-import BgLeaf2 from '../../../assets/images/bg-leaf2.png';
-import Inneraboutimg from '../../../assets/images/about-img-inner.png'
-import { Link } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
+
+import BgShape2 from "../../../assets/images/bg-shape2.png";
+import BgLeaf2 from "../../../assets/images/bg-leaf2.png";
+import { FaAward } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import team3 from '../../../assets/images/team-3.png'
+
+import { Row } from "react-bootstrap";
+
 
 
 function Profbhavnasingh() {
   return (
-
     <div className="ayur-bgcover ayur-about-sec">
-      <div className='about-bg'>
-        <div className='ayur-bread-content'>
-          <h2>About Us</h2>
+      <div className="about-bg">
+        <div className="ayur-bread-content">
+          <h2>Prof.(Dr.) Bhavna Singh, M.D. (Ayu.), Ph.D.</h2>
           <div class="ayur-bread-list">
             <span>
               <Link to="/">Home </Link>
             </span>
-            <span class="ayur-active-page">/Prof.(Dr.) Bhavna Singh, M.D. (Ayu.), Ph.D.</span>
+            <span class="ayur-active-page">/ Prof.(Dr.) Bhavna Singh, M.D. (Ayu.), Ph.D.</span>
           </div>
         </div>
       </div>
-
-
 
       <div className="row ">
         <div className="ayur-bgcover ayur-about-sec">
           <div className="container fluid about-us">
             <div className="row">
-              <div className="col-lg-6 col-md-12 col-sm-12">
-                <div className="ayur-about-img">
-                  <img
-                    src={Inneraboutimg}
-                    alt="img"
-                    data-tilt=""
-                    data-tilt-max="10"
-                    data-tilt-speed="1000"
-                    data-tilt-perspective="1000"
-                    style={{
-                      willChange: 'transform',
-                      transform:
-                        'perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)',
-                    }}
-                  />
-
+              <div className="col-lg-4 col-md-6 col-sm-6">
+                <div className="ayur-team-box">
+                  <div className="ayur-team-img-wrapper">
+                    <div className="ayur-team-img">
+                      <img src={team3} alt="team member 3" />
+                    </div>
+                    <div className="ayur-team-hoverimg">
+                      <div className="ayur-team-hoversmall">
+                        <img src={team3} alt="team member 3" />
+                      </div>
+                      <p>Research Associate</p>
+                      <div className="ayur-team-sociallink">
+                        <Link to="">{/* Facebook SVG */}</Link>
+                        <Link to="">{/* Twitter SVG */}</Link>
+                        <Link to="">{/* Github SVG */}</Link>
+                        <Link to="">{/* Instagram SVG */}</Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ayur-team-name">
+                    <h3>Prof.(Dr.) Bhavna Singh, M.D. (Ayu.), Ph.D.</h3>
+                    <p>Academician, Researcher and Author</p>
+                    <p>Research Associate</p>
+                  </div>
                 </div>
               </div>
-              <div className="col-lg-6 col-md-12 col-sm-12">
+              <div className="col-lg-8 col-md-12 col-sm-12">
                 <div className="ayur-heading-wrap ayur-about-head">
+
+
                   <h3>Trilok Ayurveda</h3>
-                  <h4>"Wellness Center and Speciality Clinic for Chronic Disorders"</h4>
-                  <div>
+                  <p>270, Nirmal Block – B, Tehri Punarvas Sthal, Pashulok, Rishikesh – 249201 (UK)
+                    475, Lane-8, Street-8, Rajendra Nagar, Dehradun – 248001 (UK)</p>
+                  <p> Ph.: +91-9837071030, +91-9758253472</p>
+                  <p>Email: singhbsbharti@gmail.com, info@trilokayurveda.com</p>
 
-                    <h5 className='pt-3'>Internal Medicine:</h5>
-                    <p>Two decades of rich clinical experience and expertise in treating, Degenerative, Auto-immune, Metabolic and other Chronic Non-Communicable Disorders (CNCD’s) through herbs based internal medicines. The key to holistic treatment is the portfolio of self manufactured 125 (and growing) herbal formulations. The purpose is to provide individualized precision medicine and to maintain the quality.</p>
-                    <h5>Wellness Program:</h5>
-                    <p>From “Illness to Wellness”, “Nirvana” is the core program of Trilok Ayurveda Wellness Center, nestled in the quaint environs of Himalayas settled at the banks of divine Ganga, dedicated to improving total health in terms of mind, body and soul detoxification and rejuvenation. It encompasses Yoga, Pranayam, Meditation, Panchkarma, Ayurvedic Dietetics and life-style related guidelines, Nadi pariksha (pulse assessment) and Ayurvedic Consultations.</p>
 
+
+
+                  <div className="pt-4">
+
+                    <h5>Educational Qualification
+                    </h5>
+                    <p>
+                      Ph.D., Oriental Studies (Ayurveda),
+                      Dev Sanskriti VishwaVidyalaya,
+                      Haridwar, Uttarakhand, India
+                      <span className="year-txt">April 2015</span>
+                    </p>
+                    <p>M.D. (Ayu.) (Ayurved Vachaspati), DravyaGuna Vigyan
+                      National Institute of Ayurveda, Jaipur,
+                      Rajasthan University, Rajasthan, India. <span className="year-txt">Oct. 2001</span></p>
+                    <p>B.A.M.S. (Bachelor of Ayurvedic Medicine and Surgery), 67.15%
+                      Rishikul State PG Ayurvedic College, Haridwar
+                      Kanpur University, UttarPradesh, India. <span className="year-txt">Oct. 1993</span></p>
                   </div>
+
+
+                </div>
+
+
+              </div>
+              <Row className="ayur-heading-wrap ayur-about-head">
+
+                <div>
+                  <h5 className="pt-4" >Thesis Title :</h5>
+                  <p>In Ph.D. – A Clinical Evaluation of Trikatu alone and with Kumari Pulp as a Hypolipidemic Drug</p>
+                  <p>In M.D. (Ayu.) – Shilajatu, Kutaki evam Khadir ka Sthaulya ke pariprekshya mein Guna-Karmatmak adhyayan</p>
+                  <h5 className="pt-2">Research Papers Published</h5>
+                  <ul>
+                    <li>1. Singh B, Upadhyay SD. Clinical Evaluation of Trikatu & Kumari as Hypolipidemic Drug. Int J Ayurveda & Med Sc 2017; 2(1): 1-7.</li>
+                    <li>Singh B, Kaur H. In silico documentation of medicinal plants in Lacchiwala range, Dehradun forest division, Uttarakhand (India). J Phytopharmacol 2018; 7(1):92-102.</li>
+                    <li>Chaudhari A, Singh B. A Critical Review of Karvira (Nerium indicum Mill). Int J Ayurveda & Med Sc 2016; 1(2): 51-55.</li>
+                    <li>Clinical evaluation of Shilajatu (Asphaltum punjabinum), Kutaki (p. kurroa), and Khadir (A. catechu) in the management of Sthaulya. Int.j.res.Ayurveda pharma;jul-aug;2013; www.ijrap.net</li>
+                  </ul>
+                  <h5 className="pt-2">Professional Experience</h5>
+                  <ul>
+                    <li> An Academician, Researcher and Author.</li>
+                    <li>Professor and HOD in the PG Department of DravyaGuna, Uttaranchal Ayurvedic Medical College, Rajpur Road, Dehradun, September 2017 onwards.</li>
+                    <li>Academician par excellence, she worked at various Institutes and held important portfolios:</li>
+                    <li>As a Resident Medical Officer at Moolchand Hospital, New Delhi.</li>
+                    <li> As an Ayurvedic Expert in Traditional Knowledge Digital Library (TKDL) project, pioneered by National Institute of Science Communication and Information Resources (NISCAIR) and Council of Scientific and Industrial Research (CSIR), New Delhi.</li>
+                    <li>As a Research Associate in Review Monograph on Indian Medicinal Plants project initiated by Indian Council of Medical Research (ICMR), New Delhi.</li>
+                    <li>Guided ten Post-Graduate research work and ongoing three studies are under her supervision.</li>
+                  </ul>
+
+                  <h5 className="pt-2">Publications</h5>
+                  <p>Authored books in the field of Ayurveda:</p>
+                  <ul>
+                    <li>Fundamental DravyaGuna – Basic Concepts of DravyaGuna and General Pharmacology</li>
+                    <li>Cancer and Medicinal Plants</li>
+                  </ul>
 
                   <Link to="#" className="ayur-btn">
                     Know More
                   </Link>
+
                 </div>
-              </div>
+
+              </Row>
             </div>
           </div>
           <div className="ayur-bgshape ayur-about-bgshape">
@@ -75,8 +140,6 @@ function Profbhavnasingh() {
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
--- src/componets/pages/about_us/Vaidyajasminesehgal.js
+++ src/componets/pages/about_us/Vaidyajasminesehgal.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React from "react";
 
 // Import images at the top
-import AboutImg from '../../../assets/images/about-img.png';
-import BgShape2 from '../../../assets/images/bg-shape2.png';
-import BgLeaf2 from '../../../assets/images/bg-leaf2.png';
-import Inneraboutimg from '../../../assets/images/about-img-inner.png'
-import { Link } from 'react-router-dom';
-import { Row } from 'react-bootstrap';
+
+import BgShape2 from "../../../assets/images/bg-shape2.png";
+import BgLeaf2 from "../../../assets/images/bg-leaf2.png";
+import { FaAward } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import team2 from '../../../assets/images/team-2.png'
+
+import { Row } from "react-bootstrap";
+const awards = [
+  {
+    icon: <FaAward className="text-warning" />,
+    title: 'Uttarakhand Gaurav',
+    year: '16th Nov. 2008'
+  },
+  {
+    icon: <FaAward className="text-warning" />,
+    title: 'Technocrat’s Excellency Award',
+    year: '23rd Dec. 2006'
+  },
+  {
+    icon: <FaAward className="text-warning" />,
+    title: 'Uttarakhand Ratna',
+    year: '1st Oct. 2006'
+  }
+];
 
 
 function Vaidyajasminesehgal() {
   return (
-
     <div className="ayur-bgcover ayur-about-sec">
-      <div className='about-bg'>
-        <div className='ayur-bread-content'>
+      <div className="about-bg">
+        <div className="ayur-bread-content">
           <h2>Vaidya Jasmine Sehgal, M.D. (Ayu.)</h2>
           <div class="ayur-bread-list">
             <span>
@@ -25,48 +43,92 @@ function Vaidyajasminesehgal() {
         </div>
       </div>
 
-
-
       <div className="row ">
         <div className="ayur-bgcover ayur-about-sec">
           <div className="container fluid about-us">
             <div className="row">
-              <div className="col-lg-6 col-md-12 col-sm-12">
-                <div className="ayur-about-img">
-                  <img
-                    src={Inneraboutimg}
-                    alt="img"
-                    data-tilt=""
-                    data-tilt-max="10"
-                    data-tilt-speed="1000"
-                    data-tilt-perspective="1000"
-                    style={{
-                      willChange: 'transform',
-                      transform:
-                        'perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)',
-                    }}
-                  />
-
+              <div className="col-lg-4 col-md-6 col-sm-6">
+                <div className="ayur-team-box">
+                  <div className="ayur-team-img-wrapper">
+                    <div className="ayur-team-img">
+                      <img src={team2} alt="team member 2" />
+                    </div>
+                    <div className="ayur-team-hoverimg">
+                      <div className="ayur-team-hoversmall">
+                        <img src={team2} alt="team member 2" />
+                      </div>
+                      <p>Director</p>
+                      <div className="ayur-team-sociallink">
+                        <Link to="">{/* Facebook SVG */}</Link>
+                        <Link to="">{/* Twitter SVG */}</Link>
+                        <Link to="">{/* Github SVG */}</Link>
+                        <Link to="">{/* Instagram SVG */}</Link>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="ayur-team-name">
+                    <h3>Vaidya Jasmine Sehgal, M.D. (Ayu.)</h3>
+                    <p>Ayurvedic Dietetics and Wellness Expert</p>
+                    <p>Director</p>
+                  </div>
                 </div>
               </div>
-              <div className="col-lg-6 col-md-12 col-sm-12">
+              <div className="col-lg-8 col-md-12 col-sm-12">
                 <div className="ayur-heading-wrap ayur-about-head">
-                  <h3>Trilok Ayurveda</h3>
-                  <h4>"Wellness Center and Speciality Clinic for Chronic Disorders"</h4>
-                  <div>
 
-                    <h5 className='pt-3'>Internal Medicine:</h5>
-                    <p>Two decades of rich clinical experience and expertise in treating, Degenerative, Auto-immune, Metabolic and other Chronic Non-Communicable Disorders (CNCD’s) through herbs based internal medicines. The key to holistic treatment is the portfolio of self manufactured 125 (and growing) herbal formulations. The purpose is to provide individualized precision medicine and to maintain the quality.</p>
-                    <h5>Wellness Program:</h5>
-                    <p>From “Illness to Wellness”, “Nirvana” is the core program of Trilok Ayurveda Wellness Center, nestled in the quaint environs of Himalayas settled at the banks of divine Ganga, dedicated to improving total health in terms of mind, body and soul detoxification and rejuvenation. It encompasses Yoga, Pranayam, Meditation, Panchkarma, Ayurvedic Dietetics and life-style related guidelines, Nadi pariksha (pulse assessment) and Ayurvedic Consultations.</p>
+
+                  <h3>Trilok Ayurveda</h3>
+                  <p>270, Nirmal Block – B, Tehri Punarvas Sthal, Pashulok, Rishikesh – 249201 (UK)
+                    475, Lane-8, Street-8, Rajendra Nagar, Dehradun – 248001 (UK)</p>
+                  <p> Ph.: +91-9758253472, +91-9837071030</p>
+                  <p> Email: jasmine@trilokayurveda.com, jasminesehgal20@gmail.com</p>
+
+
+
+
+                  <div className="pt-4">
+
+                    <h5>Educational Qualification
+                    </h5>
+                    <p>
+                      M.D. (Ayu.) (Sch.), Samhita and Sidhanta,
+                      Uttaranchal Ayurvedic Medical College, Dehradun,
+                      Uttarakhand Ayurved University, Uttarakhand, India.
+                      {/* <span className="year-txt">Nov. 2018</span> */}
+                    </p>
+                    <p>B.A.M.S. (Bachelor of Ayurvedic Medicine and Surgery), 67.70%
+                      Dayanand Ayurvedic College, Jalandhar,
+                      Guru Nanak Dev University, Punjab, India. <span className="year-txt">March 2000</span></p>
 
                   </div>
+
+
+                </div>
+
+
+              </div>
+              <Row className="ayur-heading-wrap ayur-about-head">
+
+                <div>
+                  <h5 className="pt-4" >Thesis Title :</h5>
+                  <p>A Critical Study of Ayurvedic Dietetics, in Vrihatrayi with respect to Ritu (Season)</p>
+
+                  <h5 className="pt-2">Professional Experience</h5>
+                  <ul>
+                    <li>An Ayurvedic consultant, with expertise in Ayurvedic Dietetic and Wellness since March 2000.</li>
+                  </ul>
+                  <p>Life-Style corrections are a prerequisite to treat Life-Style Disorders. An expert in Ayurvedic Dietetics and Lifestyle management, she has been instrumental in conceptualizing and initiating “Trilok Ayurveda Wellness Center” nestled in the quaint environs of Himalayas settled at the banks of divine Ganga.</p>
+                  <p>Today’s lifestyle pattern of hurry and worry and instant food has become a cause of many lifestyle related disorders, which do not have any answers in contemporary medical science.</p>
+                  <p>From “Illness to Wellness”, “Nirvana” is the core program of Trilok Ayurveda, dedicated to improving total health in terms of mind, body and soul detoxification and rejuvenation. It encompasses Yoga, Pranayam, Meditation, Panchkarma, Healthy diet and life-style related guidelines, Nadi pariksha (pulse assessment) and Ayurvedic Consultations.</p>
+                  <p>Each and every aspect of this program is tailor-made in accordance with individual constitution of ‘Tridoshas’. It is this uniqueness that has attracted people from all over the world to meet their health needs.</p>
 
                   <Link to="#" className="ayur-btn">
                     Know More
                   </Link>
+
                 </div>
-              </div>
+
+              </Row>
             </div>
           </div>
           <div className="ayur-bgshape ayur-about-bgshape">
@@ -75,8 +137,6 @@ function Vaidyajasminesehgal() {
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
