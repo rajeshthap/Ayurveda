514eb00fd6ab69da6163450056a0da5b7bcfff07
rajeshthap <jdmjvm@gmail.comgit config user.email>
2025-12-26 13:25:23 +0530

    COMING SOON

--- src/componets/CommingSoon.js
+++ src/componets/CommingSoon.js
@@ -19,12 +19,12 @@ function CommingSoon() {
     <div className="ayur-bgcover ayur-about-sec">
       <div className='about-bg'>
         <div className='ayur-bread-content'>
-          <h2 style={{ fontWeight: 'bold' }}>Comming Soon</h2>
+          <h2 style={{ fontWeight: 'bold' }}>COMING SOON</h2>
           <div className="ayur-bread-list">
             <span>
               <a href="/">Home </a>
             </span>
-            <span className="ayur-active-page">/ Comming Soon</span>
+            <span className="ayur-active-page">/ Coming Soon</span>
           </div>
         </div>
       </div>
@@ -35,13 +35,13 @@ function CommingSoon() {
             <div className="row">
               <div className="col-lg-12 col-md-12 col-sm-12">
                 <div className="ayur-heading-wrap ayur-about-head">
-                 
-                    <>
-                  <div className='text-center bg-gray-200 opacity-75 p-8 rounded-lg'>
-  <h2>Coming Soon</h2>
-</div>
-                    </>
-                
+
+                  <>
+                    <div className='text-center bg-gray-200 opacity-75 p-8 rounded-lg'>
+                      <h2>Coming Soon</h2>
+                    </div>
+                  </>
+
                 </div>
               </div>
             </div>
